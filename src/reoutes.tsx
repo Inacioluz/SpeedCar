a8a82f7f4ac23851087018ca8c49454205e6297f
Inacio Silva <joseinaciolds@gmail.com>
2024-02-28 21:40:30 -0300

    confugurando Layouts da aplicação

--- src/reoutes.tsx
+++ src/reoutes.tsx
@@ -1,8 +1,30 @@
 import { createBrowserRouter } from 'react-router-dom'
+
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
 import { Store } from './pages/app/store'
 import { SignIn } from './pages/auth/sign-in'
 
 export const router = createBrowserRouter([
-  { path: '/', element: <Store /> },
-  { path: '/sign-in', element: <SignIn /> },
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Store />,
+      },
+    ],
+  },
+
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: '/sign-in',
+        element: <SignIn />,
+      },
+    ],
+  },
 ])
