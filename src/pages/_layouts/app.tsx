ff80992db4a51bb6acd4f90b54681a8e269207d0
Inacio Silva <joseinaciolds@gmail.com>
2024-03-26 12:24:54 -0300

    navabar ok

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom'
 
-import { Header } from '@/components/header'
+import { House } from '../app/View/house'
 
 export function AppLayout() {
   return (
     <div className="flex min-h-screen flex-col antialiased">
-      <Header />
+      <House />
 
       <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
         <Outlet />
